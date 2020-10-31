31b299a1bd5a2eb55ffdbd982917ebe2bbc42d8d
Keith Paul <keith_paul@outlook.com>
2020-10-31 14:57:44 +1100

    Setup initial #2
    Installed node-sass sass and sass-loader as dev dependencies (only way to get scss to work). Also created login and signup tempalte pages and login container page

---
+++ project/src/components/UserLoginPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-form_container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLoginPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.login-form_container{
+  h1{
+    margin: 50px;
+  }
+}
+</style>
